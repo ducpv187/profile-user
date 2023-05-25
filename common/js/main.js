32b423a68a2b0a205729b0801e4e9c08b33a587f
Ducpham <ducpv@beetsoft.com>
2023-05-25 11:19:17 +0700

    update code

--- common/js/main.js
+++ common/js/main.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
   //function ClickItem => show-hidden content
-  $( ".cascade-menu .js-on" ).on( "click", function() {       
-    let innerWidthCase = $( ".cascade-menu" ).innerWidth() / 4;
-
+  $( ".cascade-menu .js-tabitem" ).on( "click", function() {       
+    let innerWidthCase = $( ".cascade-menu" ).innerWidth() / 4;    
     if($(this).hasClass("active")){
-      $(this).removeClass("active");    
+      $(this).removeClass("active");      
+      $(".tabcontent").removeClass("active-color");
+      $(".tabcontent .cascade-close").css("background-color", "");
       $( ".tabcontent" ).animate({
         width: "0",
         opacity: 0,         
@@ -12,7 +13,7 @@ $(document).ready(function() {
         $(".tabcontent").animate({                             
           width: "0",
           opacity: 0,   
-        }, 500 ,function(){                
+        }, 200 ,function(){                
           $( ".cascade-menu li:not(.active)" ).each(function() {  
             let $thisNoActive = $(this);
             let opacity ;  
@@ -29,18 +30,28 @@ $(document).ready(function() {
               }, 300);
             });
         });           
-      });                                     
+      });         
     }
     
     else {        
         $(this).addClass("active");
+        let dataColor = $(this).data('color');  
+        //change color Case-close (border)   
+        $(".tabcontent").addClass("active-color");
+        $(".tabcontent.active-color .cascade-close").css("background-color", dataColor);
+        
+        //animate showcontent   
         $( this ).animate({
           zIndex: 2,
           opacity: 1,
           left: "0",            
         }, 300, function() {
+          let dataIdTabContent = $(".tabcontent").data("tabcontent-id");
+          let dataResidual = $(".tabcontent").data("residual");
+          let widthTabContent = dataIdTabContent * innerWidthCase - dataResidual;
           $( ".tabcontent" ).animate({
-            width: "640px",
+            // width: "640px",
+            width: widthTabContent ,
             opacity: 1,         
           }, 300, function() {              
             $(".tabcontent .cascade-close").addClass("show-content");   
@@ -57,10 +68,12 @@ $(document).ready(function() {
             zIndex: 1,
             opacity: opacity,
             left: dataLeft,            
-          }, 300);
-      }); 
-    }              
+        }, 300);
+      });             
+    }          
   });
+
+
 
   //function btn Close => show-hidden content
   $( ".btn-close" ).on( "click", function() {
